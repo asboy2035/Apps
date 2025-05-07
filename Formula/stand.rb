46b2a4e6a63c4e584211ebd585e9991f3b1a9831
ash <ash@asboy2035.com>
2025-05-07 16:44:59 +0200

    switch to artifact-friendly version

--- Formula/stand.rb
+++ Formula/stand.rb
@@ -6,27 +6,21 @@ class Stand < Formula
   license "Apache-2.0"
 
   def install
-    # Unzip to the build directory first
-    system "unzip", cached_download, "-d", buildpath
+    # Create a subdirectory for the app bundle
+    prefix.install Dir["*"]
+  end
 
-    # Find the actual .app within the extracted directory
-    extracted_app_path = Dir["#{buildpath}/**/*.app"].first
-    ohai "Found extracted app at: #{extracted_app_path}"
-    target_path = "/Applications/Stand.app"
+  def caveats
+    <<~EOS
+      Stand has been installed to:
+        #{opt_prefix}/Stand.app
 
-    # Replace old version if it exists
-    if File.exist?(target_path)
-      opoo "An older version of Stand is already installed in /Applications. Replacing it..."
-      system "rm", "-rf", target_path
-    end
-
-    # Move the app to /Applications
-    system "mv", extracted_app_path, target_path
-    ohai "Stand has been moved to /Applications."
+      To make it available in the Applications folder, you can:
+        ln -s #{opt_prefix}/Stand.app /Applications/Stand.app
+    EOS
   end
 
   test do
-    # Verify that the .app was successfully installed to /Applications
-    assert_predicate Pathname("/Applications/Stand.app"), :exist?, "Stand.app was not installed to /Applications"
+    assert_predicate prefix/"Stand.app", :exist?, "Stand.app was not installed to the expected location"
   end
 end
