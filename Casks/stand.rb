411c69784e26f6be0dcdf1b5e919d4a9914d075e
ash <ash@asboy2035.com>
2025-05-07 17:45:18 +0200

    change to cask

---
+++ Casks/stand.rb
@@ -0,0 +1,9 @@
+cask stand do
+  desc "Keep track of sitting/standing time (standing desks)!"
+  homepage "https://asboy2035.com/apps/stand"
+  url "https://github.com/asboy2035/Stand/releases/download/3.5/Stand-3.5-MAS.zip"
+  sha256 "be8b6537d567502b3f857263bbea170f45f99894aae1a1e5e60a495bc943bbc1"
+  license "Apache-2.0"
+
+  app "Stand.app"
+end
